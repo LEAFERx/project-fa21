a121c7767ea2b5873b9723fcdad01251734c7915
tianye <tianye@shanghaitech.edu.cn>
2021-11-23 10:16:42 -0800

    add SA description& framework

--- SA_solver.py
+++ SA_solver.py
@@ -5,6 +5,12 @@ import math
 eps = 1
 
 def seed_sol(tasks):
+    """
+    Args:
+        tasks: list[Task], tasks[0] idx, tasks[1] deadline, tasks[2] duration, tasks[3] profit
+    Returns:
+        output: list of igloos in order of polishing  
+    """
     #greedy by profit/duration
     res = []
     n = len(tasks[0])
@@ -14,6 +20,8 @@ def seed_sol(tasks):
         ratio = 0
         idx = -1
         for j in range(n-i):
+            if tasks[2][residual_list[j]] + curr_time >= 1440:
+                continue
             exceed_time = tasks[2][residual_list[j]] + curr_time - tasks[1][residual_list[j]]
             if exceed_time > 0:
                 curr_ratio = math.exp(−0.017*exceed_time) * tasks[3][residual_list[j]]/tasks[2][residual_list[j]]
@@ -25,18 +33,28 @@ def seed_sol(tasks):
                 if curr_ratio > ratio:
                     ratio = curr_ratio
                     idx = j
-        curr_time += tasks[2][residual_list[idx]]
-        assert idx != -1    # it doesn't find a task what can be? 
-        res.append(residual_list.pop(idx))
-        if curr_time >= 1440:
+        if idx == -1:           # if all task are not valid
             res += residual_list
             break
+        else:
+            curr_time += tasks[2][residual_list[idx]]
+            res.append(residual_list.pop(idx))
+            if curr_time >= 1440:
+                res += residual_list
+                break
     return res
 
 def trans_sol(sol,tasks):
     pass
 
 def eval_sol(sol,tasks):
+    """
+    Args:
+        sol:   a list of idx of tasks to process
+        tasks: list[Task], tasks[0] idx, tasks[1] deadline, tasks[2] duration, tasks[3] profit
+    Returns:
+        output: the total profit in such solution  
+    """
     curr_time = 0
     n = len(sol)
     total_profit = 0
@@ -44,14 +62,14 @@ def eval_sol(sol,tasks):
     assert n == len(tasks[0])   #error..
 
     for i in range(n):          #iter all task
+        if tasks[2][sol[i]] + curr_time >= 1440:
+            break
         exceed_time = tasks[2][sol[i]] + curr_time - tasks[1][sol[i]]
         if exceed_time > 0:
             total_profit +=  math.exp(−0.017*exceed_time) * tasks[3][sol[i]]
         else
             total_profit +=  tasks[3][sol[i]]
-        curr_time += tasks[2][sol[i]]
-        if curr_time >= 1440:
-            break
+        curr_time += tasks[2][sol[i]]        
 
     return total_profit
 
