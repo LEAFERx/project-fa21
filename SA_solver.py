e085a14e06285872a0ef02fe7282f418161e962c
tianye <tianye@shanghaitech.edu.cn>
2021-11-28 13:20:05 -0800

    phase II preparasion

--- SA_solver.py
+++ SA_solver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.random as rd
+import Task
 import math
 
 eps = 1
@@ -7,29 +8,29 @@ eps = 1
 def seed_sol(tasks):
     """
     Args:
-        tasks: list[Task], tasks[0] idx, tasks[1] deadline, tasks[2] duration, tasks[3] profit
+        tasks: list[Task], get_task_id, get_deadline,get_duration, get_max_benefit
     Returns:
         output: list of igloos in order of polishing  
     """
     #greedy by profit/duration
     res = []
-    n = len(tasks[0])
+    n = len(tasks)
     curr_time = 0
-    residual_list = tasks[0].copy()
+    residual_list = [i for i in range(n)]
     for i in range(n):
         ratio = 0
         idx = -1
         for j in range(n-i):
-            if tasks[2][residual_list[j]] + curr_time >= 1440:
+            if tasks[residual_list[j]].get_duration() + curr_time >= 1440:
                 continue
-            exceed_time = tasks[2][residual_list[j]] + curr_time - tasks[1][residual_list[j]]
+            exceed_time = tasks[residual_list[j]].get_duration() + curr_time - tasks[residual_list[j]].get_deadline()
             if exceed_time > 0:
-                curr_ratio = math.exp(−0.017*exceed_time) * tasks[3][residual_list[j]]/tasks[2][residual_list[j]]
+                curr_ratio = tasks[residual_list[j]].get_late_benefit(exceed_time)
                 if curr_ratio > ratio:
                     ratio = curr_ratio
                     idx = j
-            else
-                curr_ratio = tasks[3][residual_list[j]]/tasks[2][residual_list[j]]
+            else:
+                curr_ratio = tasks[residual_list[j]].get_max_benefit()/tasks[residual_list[j]].get_duration()
                 if curr_ratio > ratio:
                     ratio = curr_ratio
                     idx = j
@@ -37,8 +38,8 @@ def seed_sol(tasks):
             res += residual_list
             break
         else:
-            curr_time += tasks[2][residual_list[idx]]
-            res.append(residual_list.pop(idx))
+            curr_time += tasks[residual_list[idx]].get_duration()
+            res.append(residual_list.pop(idx)+1)
             if curr_time >= 1440:
                 res += residual_list
                 break
@@ -66,8 +67,8 @@ def eval_sol(sol,tasks):
             break
         exceed_time = tasks[2][sol[i]] + curr_time - tasks[1][sol[i]]
         if exceed_time > 0:
-            total_profit +=  math.exp(−0.017*exceed_time) * tasks[3][sol[i]]
-        else
+            total_profit +=  math.exp(-0.017*exceed_time) * tasks[3][sol[i]]
+        else:
             total_profit +=  tasks[3][sol[i]]
         curr_time += tasks[2][sol[i]]        
 
--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 from parse import read_input_file, write_output_file
 import os
+import SA_solver as sa
 
 def solve(tasks):
     """
@@ -8,13 +9,23 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
-    pass
+    sol = sa.seed_sol(tasks)
+    revised_sol = []
+    curr_time = 0
+    for i in range(len(sol)):          #iter all task
+        if tasks[sol[i]].get_duration() + curr_time >= 1440:
+            break
+        curr_time += tasks[sol[i]].get_duration()
+        revised_sol.append(sol[i])
+
+    return revised_sol
+
 
 
 # Here's an example of how to run your solver.
-# if __name__ == '__main__':
-#     for input_path in os.listdir('inputs/'):
-#         output_path = 'outputs/' + input_path[:-3] + '.out'
-#         tasks = read_input_file(input_path)
-#         output = solve(tasks)
-#         write_output_file(output_path, output)
+if __name__ == '__main__':
+    for input_path in os.listdir('inputs/'):
+        output_path = 'outputs/' + input_path[:-3] + '.out'
+        tasks = read_input_file('inputs/'+input_path)
+        output = solve(tasks)
+        write_output_file(output_path, output)
