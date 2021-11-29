959534b9ffc02d086a3b55603d943e08d7e44839
WCY2000 <2431873699@qq.com>
2021-11-28 23:07:41 -0800

    add seed_sol untested

--- sa.cpp
+++ sa.cpp
@@ -2,10 +2,50 @@
 
 Solution seed_sol(const std::vector<Task> tasks) {
     auto sol = Solution();
-    sol.sols = {1, 2, 3, 4};
-    sol.end = 2;
+    // sol.sols = {1, 2, 3, 4};
+    // sol.end = 2;
+    // return sol;
+    int n = tasks.size();
+    float curr_time = 0;
+    std::vector<int> residual_list;
+    for (int i = 0; i < n; i++){
+        residual_list[i] = i+1; 
+    }
+    for (int i = 0; i < n ; i++){
+        float ratio = 0;
+        int idx = -1;
+        for (int j = 0 ; j < n - i; j++){
+            if (tasks[residual_list[j]-1].duration + curr_time >=1440)
+            continue;
+            auto tsk = tasks[residual_list[j]-1];
+            float exceed_time = tsk.duration + curr_time - tsk.deadline;
+            float curr_ratio = tsk.get_late_benefit(exceed_time);
+            if (curr_ratio > ratio){
+                ratio = curr_ratio;
+                idx = j;
+            }
+        }
+        if (idx == -1){        // if all task are not valid
+            for (int k = sol.end; k < sol.end + n; k++ ){
+            sol.sols[k] = residual_list[k-sol.end];
+            sol.end ++;
+            }
+            i -= 1;
+            break;
+        }
+
+        else{
+            curr_time += tasks[residual_list[idx]-1].duration;
+            sol.sols[sol.end] = residual_list[idx];
+            residual_list.erase(residual_list.begin()+idx-1);
+        }
+
+    }
     return sol;
 }
+
+
+
 
 void trans_sol(Solution& sol, const std::vector<Task> tasks) {
     return;
--- sa.h
+++ sa.h
@@ -3,12 +3,18 @@
 
 #include <vector>
 #include <utility>
-
+#include <cmath>
 struct Task {
     int task_id;
     int deadline;
     int duration;
     float perfect_benefit;
+    
+    float get_late_benefit(float minutes_late){
+        float m_minutes_late = std::max(float(0), minutes_late); 
+        return  perfect_benefit * exp(-0.0170 * m_minutes_late);
+    }
+
 };
 
 class Solution {
